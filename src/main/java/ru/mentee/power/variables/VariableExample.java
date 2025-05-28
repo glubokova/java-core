e2f8c44264ca01e1ae50a842d9df3432256cd7dc
Глубокова Аида Владимировна <glubokova_aida@mail.ru>
2025-05-28 22:16:53 +0500

    MP-18 Переменные в Java: основы

---
+++ src/main/java/ru/mentee/power/variables/VariableExample.java
@@ -0,0 +1,11 @@
+package ru.mentee.power.variables;
+
+import static java.lang.System.*;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        int myAge = 23;
+        String myName = "Aida";
+        System.out.println("My name is" + " " + myName + ", Im " + myAge + " years old.");
+    }
+}
